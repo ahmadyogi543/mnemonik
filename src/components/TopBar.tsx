87a99ddc3dd6ff0b83a02a6b3333fda4c38f8c6d
ahmadyogi543 <ahmadyogi543@gmail.com>
2024-08-15 15:09:59 +0800

    update: menambahkan icon asli pada bar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,9 +1,17 @@
+import { MdMenu, MdMoreVert } from "react-icons/md";
+
 const TopBar = () => {
   return (
     <div className="flex justify-between p-5 bg-zinc-800 text-slate-100">
-      <div>{"[burger]"}</div>
-      <div>Mnemonik</div>
-      <div>{"[dots]"}</div>
+      <div>
+        <MdMenu className="text-2xl" />
+      </div>
+      <div>
+        <h1>Mnemonik</h1>
+      </div>
+      <div>
+        <MdMoreVert className="text-2xl" />
+      </div>
     </div>
   );
 };
