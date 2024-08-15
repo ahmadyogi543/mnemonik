f3344936b3f49c18546c8ea358a738027bfd3b94
ahmadyogi543 <ahmadyogi543@gmail.com>
2024-08-15 21:40:39 +0800

    update: make the title text on button bold

--- src/components/sidebar/AddNoteButton.tsx
+++ src/components/sidebar/AddNoteButton.tsx
@@ -4,7 +4,7 @@ const AddNoteButton = () => {
   return (
     <button className="flex items-center justify-center flex-grow p-4 bg-zinc-800 text-white text-sm font-medium">
       <FaPlus className="me-4" />
-      <span>Tambah Catatan Baru</span>
+      <span className="font-bold">Tambah Catatan Baru</span>
     </button>
   );
 };
