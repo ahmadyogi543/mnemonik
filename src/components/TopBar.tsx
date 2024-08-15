dfa6550d90cd567cc57b05b6549df5d1daceba60
ahmadyogi543 <ahmadyogi543@gmail.com>
2024-08-15 15:17:06 +0800

    update: menambahkan shadow dan mengubah judul

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -2,12 +2,12 @@ import { MdMenu, MdMoreVert } from "react-icons/md";
 
 const TopBar = () => {
   return (
-    <div className="flex justify-between p-5 bg-zinc-800 text-slate-100">
+    <div className="flex justify-between p-5 shadow-md bg-zinc-800 text-slate-100">
       <div>
         <MdMenu className="text-2xl" />
       </div>
       <div>
-        <h1 className="font-bold">Mnemonik</h1>
+        <h1 className="font-bold">Mnemonik: A Note-taking App</h1>
       </div>
       <div>
         <MdMoreVert className="text-2xl" />
