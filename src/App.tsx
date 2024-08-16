506d5d2b3ad952bd844aaec7230e82eb151b45b4
ahmadyogi543 <ahmadyogi543@gmail.com>
2024-08-16 13:47:22 +0800

    update: put the Context into the game!

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import { NoteContextProvider } from "./context/NoteContext";
+
 import Body from "./components/Body";
 import Topbar from "./components/Topbar";
 
 const App = () => {
   return (
-    <>
+    <NoteContextProvider>
       <Topbar />
       <Body />
-    </>
+    </NoteContextProvider>
   );
 };
 
