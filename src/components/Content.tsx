349d3b0f57c756deecaff168ab28ca82f6da74a6
ahmadyogi543 <ahmadyogi543@gmail.com>
2024-08-16 08:53:14 +0800

    update: add illustration when the note is empty

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,5 @@
+// import NotesBro from "../assets/images/notes-bro.svg";
+
 const Content = () => {
   return (
     <div className="flex flex-col flex-grow bg-white border border-gray-300">
@@ -10,6 +12,9 @@ const Content = () => {
         className="focus:outline-none resize-none flex-grow block p-8"
         placeholder="Konten..."
       ></textarea>
+      {/* <div className="flex flex-col flex-grow items-center justify-center">
+          <img width={360} src={NotesBro} alt="illustration" />
+        </div> */}
     </div>
   );
 };
