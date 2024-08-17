6da5892b6cbac432d9891f5d5a3e438e8453e748
ahmadyogi543 <ahmadyogi543@gmail.com>
2024-08-17 21:02:28 +0800

    update: use the db name instead it's path

--- server/src/config.ts
+++ server/src/config.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config({ path: ".env.local" });
 
 const config = {
-  DB_PATH: process.env.DB_PATH || ":memory:",
+  DB_NAME: `src/data/bin/${process.env.DB_NAME}` || ":memory:",
   HOSTNAME: process.env.HOSTNAME || "localhost",
   PORT: process.env.PORT || 3000,
 };
