341fcf38d4f39890ce75c996d404167e1e7c759b
ahmadyogi543 <ahmadyogi543@gmail.com>
2024-08-17 12:05:51 +0800

    update: add migrations to insert some example notes

---
+++ server/src/data/migrations/insert_notes_table_up.sql
@@ -0,0 +1,8 @@
+INSERT INTO notes (title, body, createdAt, updatedAt)
+VALUES ('First Note', 'This is the body of the first note.', '2024-08-01 10:00:00', '2024-08-01 10:00:00');
+
+INSERT INTO notes (title, body)
+VALUES ('Second Note', 'This is the body of the second note.');
+
+INSERT INTO notes (title, body, createdAt)
+VALUES ('Third Note', 'This is the body of the third note.', '2024-08-02 15:00:00');
