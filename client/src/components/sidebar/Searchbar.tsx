beafc763328f5a878de4b8436c593212dd93cc04
ahmadyogi543 <ahmadyogi543@gmail.com>
2024-08-17 22:07:48 +0800

    update: implement the searchbar functionality

--- client/src/components/sidebar/Searchbar.tsx
+++ client/src/components/sidebar/Searchbar.tsx
@@ -1,20 +1,27 @@
-import { FaSearch } from "react-icons/fa";
+import { useState } from "react";
+import { FaTimes, FaSearch } from "react-icons/fa";
 
 const Searchbar = () => {
+  const [query, setQuery] = useState("");
+
+  const handleOnClickBtn = () => {
+    setQuery("");
+  };
+
   return (
     <div className="flex gap-2">
       <input
         className="flex-grow focus:outline-none block px-4 py-2 border border-gray-300 text-sm"
         placeholder="Cari catatan berdasarkan judul..."
         type="text"
-        name=""
-        id=""
+        onChange={(ev) => setQuery(ev.target.value)}
+        value={query}
       />
       <button
         className="block px-3 bg-zinc-800 hover:bg-zinc-700 text-white text-sm"
-        title="Cari"
+        onClick={handleOnClickBtn}
       >
-        <FaSearch />
+        {query === "" ? <FaSearch /> : <FaTimes />}
       </button>
     </div>
   );
